2a0e9fc73cfe9e31a963d7312dc2d84238b7f380
zeko450 <114595632+zeko450@users.noreply.github.com>
2022-11-26 23:44:56 -0500

    Update VoitureController.java

--- VoitureController.java
+++ VoitureController.java
@@ -2,6 +2,8 @@ package com.example.locationvoiture.controller;
 
 import com.example.locationvoiture.entity.Voiture;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
