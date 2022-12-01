cd9ad3c34cd9380f8ead933fb65dd929d300c53f
zeko450 <114595632+zeko450@users.noreply.github.com>
2022-11-30 22:23:21 -0500

    Update VoitureRepository.java

--- VoitureRepository.java
+++ VoitureRepository.java
@@ -2,7 +2,12 @@ package com.example.locationvoiture.repository;
 
 import com.example.locationvoiture.entity.Voiture;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface VoitureRepository extends JpaRepository<Voiture, Integer> {
+  @Query("SELECT v from Voiture v WHERE v.marque LIKE %?1%"
+            + " OR v.modele LIKE %?1%"
+            + "OR v.couleur LIKE %?1%")
+    List<Voiture> findAll(String keyword);
 }
 
